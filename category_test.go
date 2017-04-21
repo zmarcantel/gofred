a810523bbdd9d89514c07bbdc9be86ef2d00799b
Zach Marcantel <zmarcantel@gmail.com>
2017-04-20 23:58:41 -0500

    try using yesterday

--- category_test.go
+++ category_test.go
@@ -67,7 +67,7 @@ func TestCategoryChildren_TradeBalance(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	children, err := client.CategoryChildren(trade.ParentId, time.Unix(0, 0), time.Now().Local())
+	children, err := client.CategoryChildren(trade.ParentId, time.Unix(0, 0), time.Now().Add(-time.Hour*24))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -96,7 +96,7 @@ func TestCategoryChildren_TradeBalance(t *testing.T) {
 
 func TestCategoryRelated_Districts(t *testing.T) {
 	client := make_client(t)
-	related, err := client.RelatedCategories(CATEGORY_STLOUIS_DISTRICT_STATES, time.Unix(0, 0), time.Now().Local())
+	related, err := client.RelatedCategories(CATEGORY_STLOUIS_DISTRICT_STATES, time.Unix(0, 0), time.Now().Add(-time.Hour*24))
 	if err != nil {
 		t.Fatal(err)
 	}
